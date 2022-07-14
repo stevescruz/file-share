fbc03388a0bd3d512863997b15ca056513b6befb
steve <steve.silverio.cruz@gmail.com>
2022-07-14 02:14:33 -0700

    Added project's directory, server, routes and views constants

---
+++ src/shared/constants.js
@@ -0,0 +1,24 @@
+const DIR_CONSTANTS = {
+  public: "/public",
+  css: "/css",
+  js: "/js",
+  img: "/img",
+  views: "/views",
+};
+
+const SERVER_CONSTANTS = {
+  defaultTemplateEngine: "ejs",
+}
+
+const ROUTES_CONSTANTS = {
+  files: "/files",
+  upload: "/upload",
+  download: "/download",
+}
+
+const VIEWS_CONSTANTS = {
+  fileDownload: "filedownload",
+  fileUpload: "fileupload",
+}
+
+export { DIR_CONSTANTS, SERVER_CONSTANTS, ROUTES_CONSTANTS, VIEWS_CONSTANTS };
