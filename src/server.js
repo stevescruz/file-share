52d5cbfcec58dfb3e848cf0b50c89ef93086fc97
steve <steve.silverio.cruz@gmail.com>
2022-09-11 20:34:04 -0700

    Set up app's template engine and static files directories

--- src/server.js
+++ src/server.js
@@ -4,10 +4,18 @@ import express from "express";
 
 import initDb from "./database/initDb.js";
 import filesRouter from "./routes/filesRoutes.js";
-import { ROUTES } from "./shared/constants.js";
+import { DIR, PATHS, SERVER, ROUTES } from "./shared/constants.js";
 
 const app = express();
 const port = process.env.PORT;
+
+app.use(express.static(DIR.public));
+app.use(DIR.css, express.static(PATHS.toCssDir));
+app.use(DIR.js, express.static(PATHS.toJsDir));
+app.use(DIR.img, express.static(PATHS.toImgDir));
+
+app.set("views", PATHS.toViewsDir);
+app.set("view engine", SERVER.defaultTemplateEngine);
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
