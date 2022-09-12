2cc53fa9875b4beb15347b62aadd274251f61232
steve <steve.silverio.cruz@gmail.com>
2022-09-11 20:29:59 -0700

    Set app server port with express

---
+++ src/server.js
@@ -0,0 +1,10 @@
+import dotenv from "dotenv";
+dotenv.config();
+import express from "express";
+
+const app = express();
+const port = process.env.PORT;
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
